9002d1d53905880bb96a2c33cf47d9a703daefe8
Martin Trumann <martin.trumann@pm.me>
2020-10-10 20:57:44 +0300

    fix

--- index.js
+++ index.js
@@ -29,16 +29,17 @@ app.get('/:type/:id', (req, res) => {
 
 	url += "&from=" + from.format("YYYY-MM-DDT00:00:00") + "Z" +
 		"&thru=" + thru.format("YYYY-MM-DDT00:00:00") + "Z" +
-		"&lang=et"
+		"&lang=ET"
 
 	fetch(url)
 		.then(raw => raw.json())
 		.then(data => {
 			res.append( 'Content-Type', 'text/calendar')
-			res.send(data.content.replace(/BEGIN:VEVENT\r/g, "BEGIN:VEVENT\r\nDTSTAMP:" + time.toISOString().replace(/[-.:Z]/g, "")))
+			res.send(data.content)
 		})
 		.catch(err => {
 			res.send("Invalid id")
+			console.error(err);
 		})
 })
 
